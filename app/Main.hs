96d60890af1bbb7d9f4049a9d79ae66c15848338
Xuanda Yang <th3charlie@gmail.com>
2021-12-02 01:29:04 -0800

    day 2

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Lib ( day1Part1Solution, day1Part2Solution )
+import Lib ( day1Part1Solution, day1Part2Solution, day2Part1Solution, day2Part2Solution )
 
 main :: IO ()
-main = day1Part2Solution
+main = day2Part2Solution
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,9 @@
 module Lib
     (
         day1Part1Solution,
-        day1Part2Solution
+        day1Part2Solution,
+        day2Part1Solution,
+        day2Part2Solution
     ) where
 
 import System.Environment ( getArgs )
@@ -40,3 +42,53 @@ day1Part2Solution = do
     let numbers = getNumbers contents
     let increase = countIncreasedMeasurementWindow numbers
     print increase
+
+getCommand :: [String] -> [(String, Int)]
+getCommand ls =
+    let tokenized = map words ls in
+        [(head x, read (last x) :: Int) | x <- tokenized]
+
+getPositionOffset :: (String, Int) -> Int
+getPositionOffset ("forward", x)= x
+getPositionOffset _ = 0
+
+getDepthOffset :: (String, Int) -> Int
+getDepthOffset ("down", x) = x
+getDepthOffset ("up", x)   = -x
+getDepthOffset _   = 0
+
+
+getPosition :: [(String, Int)] -> Int
+getPosition ls = sum $ map getPositionOffset ls
+
+getDepth :: [(String, Int)] -> Int
+getDepth ls = sum $ map getDepthOffset ls
+
+day2Part1Solution :: IO ()
+day2Part1Solution = do
+    args <- getArgs
+    let inputFile = head args
+    contents <- readFile inputFile
+    let ls = lines contents
+    let commands = getCommand ls
+    let pos = getPosition commands
+    let depth = getDepth commands
+    print (pos * depth)
+
+getDepthWithAim :: [(String, Int)] -> Int
+getDepthWithAim ls = res where
+    aimChanges = map getPositionOffset ls
+    depthOffsets = map getDepthOffset ls
+    aim = scanl1 (+) depthOffsets
+    res = sum [i * j | (i, j) <- zip aimChanges aim]
+
+day2Part2Solution :: IO ()
+day2Part2Solution = do
+    args <- getArgs
+    let inputFile = head args
+    contents <- readFile inputFile
+    let ls = lines contents
+    let commands = getCommand ls
+    let pos = getPosition commands
+    let depth = getDepthWithAim commands
+    print (pos * depth)
